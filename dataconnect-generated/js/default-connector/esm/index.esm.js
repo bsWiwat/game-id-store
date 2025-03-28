6e6e8ee57706b89062782a11b9fe9c52376508d0
bsWiwat <bs.wiwat@gmail.com>
2025-03-28 09:59:23 +0700

    setup(firebase): bib for db, storage, and auth

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'game-id-store',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'game-id-store',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
