8db0f640f653098effc8d6d70fabdd4b88f0d8fb
bsWiwat <bs.wiwat@gmail.com>
2025-03-29 13:33:54 +0700

    Update(web): update to use api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+// import { AuthProvider } from "@/context/AuthContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
+        <Navbar />
+        {/* <AuthProvider>{children}</AuthProvider> */}
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
