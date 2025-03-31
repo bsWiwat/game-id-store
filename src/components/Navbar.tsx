4a56dc73474c34ae1e32c5cf1d767ab05e108a04
ntwira <s6604062630536@email.kmutnb.ac.th>
2025-03-31 21:09:59 +0700

    Change Category list

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,9 +39,8 @@ const Navbar = () => {
                 Homepage
               </Link>
               <Link href="/list">Shop</Link>
-              <Link href="/">Deals</Link>
               <Link href="/">About</Link>
-              <Link href="/">Contract</Link>
+              <Link href="/">Contact</Link>
             </div>
           </div>
           {/* Right */}
